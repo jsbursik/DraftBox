35a1d5199b2a32eec7e22d4d9a08b95341ef4a82
Jordan Bursik <jsbursik@gmail.com>
2025-10-21 18:54:51 -0500

    Getting ready for scryfall data upload

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import FileInput from "$lib/components/form-components/FileInput.svelte";
+</script>
+
+<div class="center">
+  <div class="card">
+    <h2 class="center-text">Admin Stuff</h2>
+    <div class="card" style="width: fit-content">
+      <form class="stack" action="POST">
+        <FileInput name="scryfall-data" label="Upload Scryfall data" accept=".json" required />
+        <button type="submit" style="width: 100%">Upload</button>
+      </form>
+    </div>
+  </div>
+</div>
